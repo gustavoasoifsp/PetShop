592c6b996355a5a96e4ed9bd0daa28cec78d1121
gustavoaso <gustavo.alcantara@aluno.ifsp.edu.br>
2026-03-25 08:38:49 -0300

    Finalizando projeto

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet'
 
 export const search = (req: Request, res: Response) => {
+	let query: string = req.query.q as string
+	if(!query)
 	res.send('Buscando.....');
 	// res.render('pages/search');
 }
